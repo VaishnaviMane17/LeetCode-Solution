03c777e4242efe990bb3e2c165de3066933ebb7e
Vaishnavi Vishwas Mane <112743088+VaishnaviMane17@users.noreply.github.com>
2025-11-08 21:12:55 -0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (28.89%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -7,12 +7,19 @@
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
         if not root:
-            return
-
-        root.left = self.removeLeafNodes(root.left, target) 
-        root.right = self.removeLeafNodes(root.right, target)
-        if not root.left and not root.right and root.val == target:
             return None
-        
+
+    # Recurse and update the children
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+    # Now check if this node became a leaf with target value
+        if not root.left and not root.right and root.val == target:
+            return None  # delete this node
+
         return root
+
+
+
+        
         
